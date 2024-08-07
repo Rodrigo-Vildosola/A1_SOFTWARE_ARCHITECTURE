2c55a7fdb078ea8ccb464718ea86fbc7924d1f0a
Rodrigo-Vildosola <rjvildosola@miuandes.cl>
2024-08-07 10:04:20 -0400

    Init commit

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,25 +1,54 @@
 from django import forms
 from .models import Author, Book, Review, SalesByYear
 
-class AuthorForm(forms.ModelForm):
-    class Meta:
-        model = Author
-        fields = '__all__'
+class AuthorForm(forms.Form):
+    name = forms.CharField(max_length=255)
+    date_of_birth = forms.DateField()
+    country_of_origin = forms.CharField(max_length=255)
+    short_description = forms.CharField(widget=forms.Textarea)
 
+    def save(self, commit=True):
+        data = self.cleaned_data
+        author = Author(**data)
+        if commit:
+            author.save()
+        return author
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = '__all__'
+class BookForm(forms.Form):
+    name = forms.CharField(max_length=255)
+    summary = forms.CharField(widget=forms.Textarea)
+    date_of_publication = forms.DateField()
+    number_of_sales = forms.IntegerField()
+    author = forms.ModelChoiceField(queryset=Author.objects.all())
 
+    def save(self, commit=True):
+        data = self.cleaned_data
+        book = Book(**data)
+        if commit:
+            book.save()
+        return book
 
-class ReviewForm(forms.ModelForm):
-    class Meta:
-        model = Review
-        fields = '__all__'
+class ReviewForm(forms.Form):
+    book = forms.ModelChoiceField(queryset=Book.objects.all())
+    review = forms.CharField(widget=forms.Textarea)
+    score = forms.IntegerField(min_value=1, max_value=5)
+    number_of_upvotes = forms.IntegerField()
 
+    def save(self, commit=True):
+        data = self.cleaned_data
+        review = Review(**data)
+        if commit:
+            review.save()
+        return review
 
-class SalesByYearForm(forms.ModelForm):
-    class Meta:
-        model = SalesByYear
-        fields = '__all__'
+class SalesByYearForm(forms.Form):
+    book = forms.ModelChoiceField(queryset=Book.objects.all())
+    year = forms.IntegerField()
+    sales = forms.IntegerField()
+
+    def save(self, commit=True):
+        data = self.cleaned_data
+        sales_by_year = SalesByYear(**data)
+        if commit:
+            sales_by_year.save()
+        return sales_by_year
--- reviews/models.py
+++ reviews/models.py
@@ -1,40 +1,38 @@
-from django.db import models
+from mongoengine import Document, fields
+from django.utils import timezone
 
-class Author(models.Model):
-    name = models.CharField(max_length=255)
-    date_of_birth = models.DateField()
-    country_of_origin = models.CharField(max_length=255)
-    short_description = models.TextField()
-
-    def __str__(self):
-        return self.name
-
-
-class Book(models.Model):
-    name = models.CharField(max_length=255)
-    summary = models.TextField()
-    date_of_publication = models.DateField()
-    number_of_sales = models.IntegerField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+class Author(Document):
+    name = fields.StringField(max_length=255, required=True)
+    date_of_birth = fields.DateField(required=True)
+    country_of_origin = fields.StringField(max_length=255, required=True)
+    short_description = fields.StringField()
 
     def __str__(self):
         return self.name
 
+class Book(Document):
+    name = fields.StringField(max_length=255, required=True)
+    summary = fields.StringField()
+    date_of_publication = fields.DateField(required=True)
+    number_of_sales = fields.IntField()
+    author = fields.ReferenceField(Author, reverse_delete_rule=fields.CASCADE)
 
-class Review(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
-    review = models.TextField()
-    score = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])
-    number_of_upvotes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.name
+
+class Review(Document):
+    book = fields.ReferenceField(Book, reverse_delete_rule=fields.CASCADE)
+    review = fields.StringField()
+    score = fields.IntField(choices=[1, 2, 3, 4, 5], required=True)
+    number_of_upvotes = fields.IntField(default=0)
 
     def __str__(self):
         return f"{self.book.name} - {self.score}"
 
-
-class SalesByYear(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='yearly_sales')
-    year = models.IntegerField()
-    sales = models.IntegerField()
+class SalesByYear(Document):
+    book = fields.ReferenceField(Book, reverse_delete_rule=fields.CASCADE)
+    year = fields.IntField(required=True)
+    sales = fields.IntField(required=True)
 
     def __str__(self):
         return f"{self.book.name} - {self.year}"
