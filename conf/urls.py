fb072ca9491b315bd925398cf5e5fc822226d938
Rodrigo-Vildosola <rjvildosola@miuandes.cl>
2024-08-13 12:33:36 -0400

    Added pagination to books

--- conf/urls.py
+++ conf/urls.py
@@ -1,7 +1,15 @@
+# conf/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reviews.urls')),
 ]
+
+
+if settings.DEBUG:
+    print("here", settings.STATIC_URL)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
